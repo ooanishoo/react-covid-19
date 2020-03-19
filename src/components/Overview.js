b0271e3dab1b98c5ec46c14559dd7d64dd921f35
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-19 17:29:40 +1100

    Add colors to the cases for overview section

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,38 +2,62 @@ import React from "react";
 import GetStats from "../utils/GetStats";
 
 export default function Overview() {
-  const url = "https://corona.lmao.ninja/all";
+  //const url = "https://corona.lmao.ninja/all";
+  const url = "https://covid19.mathdro.id/api";
   const data = GetStats(url);
+
+  const styles = {
+    container: {
+      backgroundColor: "#2A282B",
+      color:"white"
+    },
+    title: { textAlign: "center", marginTop:0, paddingTop:10 },
+    cases: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      flexWrap: "wrap",
+      backgroundColor: "#2A282B"
+    },
+    case: {
+    },
+    confirmed:{
+      color:"#F98705",
+      textAlign: "center"
+    },
+    deaths:{
+      color:"#DC3545",
+      textAlign: "center"
+    },
+    recovered:{
+      color:"#28A745",
+      textAlign: "center"
+    }
+  };
 
   if (!data) return <div>Loading...</div>;
   return (
-    <div>
-      <div style={{ textAlign: "center" }}>
+    <div style={styles.container}>
+      <div style={styles.title}>
         <h1>Overview</h1>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around"
-        }}
-      >
-        <div>
-          <h3>Cases</h3>
-          <span>{data.cases}</span>
+      <div style={styles.cases}>
+        <div style={styles.confirmed}>
+          <p>Confirmed</p>
+          <h1>{data.confirmed.value}</h1>
         </div>
-        <div>
-          <h3>Deaths</h3>
-          <span>{data.deaths}</span>
+        <div style={styles.deaths}>
+          <p>Deaths</p>
+          <h1>{data.deaths.value}</h1>
         </div>
-        <div>
-          <h3>Recovered</h3>
-          <span>{data.recovered}</span>
+        <div style={styles.recovered}>
+          <p>Recovered</p>
+          <h1>{data.recovered.value}</h1>
         </div>
-        <div>
+        {/* <div>
           <h3>Updated</h3>
           <span>{data.updated}</span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
