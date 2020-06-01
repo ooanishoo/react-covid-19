4da3c3769eb2e5a5cbb1fbb88c8f982e52e0a638
ooanishoo <anish.maharjan12341@gmail.com>
2020-06-01 19:11:33 +1000

    Updates

---
+++ precache-manifest.35492c1eaa7310a30e7557d3a67ef69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb14bf85e85c6ee69a69dc72bef99c1b",
+    "url": "/ooanishoo/react-covid-19/index.html"
+  },
+  {
+    "revision": "2ad91e3f3b9aeea4aff2",
+    "url": "/ooanishoo/react-covid-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eeb8890580249f087646",
+    "url": "/ooanishoo/react-covid-19/static/js/2.c9a823d5.chunk.js"
+  },
+  {
+    "revision": "432ed034d9831dc13cf75201c058fd1d",
+    "url": "/ooanishoo/react-covid-19/static/js/2.c9a823d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad91e3f3b9aeea4aff2",
+    "url": "/ooanishoo/react-covid-19/static/js/main.a9253c51.chunk.js"
+  },
+  {
+    "revision": "affb92e12309c523cdc1",
+    "url": "/ooanishoo/react-covid-19/static/js/runtime-main.dac88b05.js"
+  }
+]);
